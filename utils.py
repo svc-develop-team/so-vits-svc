812a2b3741bbf0ea8855959ee9c3cbb2e487e436
ylzz1997 <ylzz1997@outlook.com>
2023-04-10 13:54:04 +0800

    Updata 768-vec

--- utils.py
+++ utils.py
@@ -236,8 +236,7 @@ def get_hubert_content(hmodel, wav_16k_tensor):
   }
   with torch.no_grad():
     logits = hmodel.extract_features(**inputs)
-    feats = hmodel.final_proj(logits[0])
-  return feats.transpose(1, 2)
+  return logits[0].transpose(1, 2)
 
 
 def get_content(cmodel, y):
