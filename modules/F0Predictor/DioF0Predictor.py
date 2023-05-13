a54b0412282a39833e019bc06c0ad4fd7635a03c
YuriHead <ylzz1997@outlook.com>
2023-05-14 00:44:57 +0800

    Update DioF0Predictor.py

--- modules/F0Predictor/DioF0Predictor.py
+++ modules/F0Predictor/DioF0Predictor.py
@@ -1,7 +1,6 @@
 from modules.F0Predictor.F0Predictor import F0Predictor
 import pyworld
 import numpy as np
-import scipy
 
 class DioF0Predictor(F0Predictor):
     def __init__(self,hop_length=512,f0_min=50,f0_max=1100,sampling_rate=44100):
@@ -10,23 +9,50 @@ class DioF0Predictor(F0Predictor):
         self.f0_max = f0_max
         self.sampling_rate = sampling_rate
 
+    def interpolate_f0(self,f0):
+        '''
+        对F0进行插值处理
+        '''
+    
+        data = np.reshape(f0, (f0.size, 1))
+    
+        vuv_vector = np.zeros((data.size, 1), dtype=np.float32)
+        vuv_vector[data > 0.0] = 1.0
+        vuv_vector[data <= 0.0] = 0.0
+    
+        ip_data = data
+    
+        frame_number = data.size
+        last_value = 0.0
+        for i in range(frame_number):
+            if data[i] <= 0.0:
+                j = i + 1
+                for j in range(i + 1, frame_number):
+                    if data[j] > 0.0:
+                        break
+                if j < frame_number - 1:
+                    if last_value > 0.0:
+                        step = (data[j] - data[i - 1]) / float(j - i)
+                        for k in range(i, j):
+                            ip_data[k] = data[i - 1] + step * (k - i + 1)
+                    else:
+                        for k in range(i, j):
+                            ip_data[k] = data[j]
+                else:
+                    for k in range(i, frame_number):
+                        ip_data[k] = last_value
+            else:
+                ip_data[i] = data[i] #这里可能存在一个没有必要的拷贝
+                last_value = data[i]
+    
+        return ip_data[:,0], vuv_vector[:,0]
+
     def resize_f0(self,x, target_len):
         source = np.array(x)
         source[source<0.001] = np.nan
         target = np.interp(np.arange(0, len(source)*target_len, len(source))/ target_len, np.arange(0, len(source)), source)
         res = np.nan_to_num(target)
         return res
-    
-    def resize_f0_uv(self,x, target_len):
-        source = np.array(x)
-        vuv_vector = np.zeros_like(x)
-        vuv_vector[x > 0.0] = 1.0
-        vuv_vector[x < 0.001] = 0.0
-        source[source<0.001] = np.nan
-        target = np.interp(np.arange(0, len(source)*target_len, len(source))/ target_len, np.arange(0, len(source)), source)
-        res = np.nan_to_num(target)
-        vuv_vector = np.ceil(scipy.ndimage.zoom(vuv_vector,target_len/len(vuv_vector),order = 0))
-        return res,vuv_vector
         
     def compute_f0(self,wav,p_len=None):
         if p_len is None:
@@ -41,7 +67,7 @@ class DioF0Predictor(F0Predictor):
         f0 = pyworld.stonemask(wav.astype(np.double), f0, t, self.sampling_rate)
         for index, pitch in enumerate(f0):
             f0[index] = round(pitch, 1)
-        return self.resize_f0(f0, p_len)
+        return self.resize_f0(f0, p_len)[0]
 
     def compute_f0_uv(self,wav,p_len=None):
         if p_len is None:
@@ -56,4 +82,4 @@ class DioF0Predictor(F0Predictor):
         f0 = pyworld.stonemask(wav.astype(np.double), f0, t, self.sampling_rate)
         for index, pitch in enumerate(f0):
             f0[index] = round(pitch, 1)
-        return self.resize_f0_uv(f0, p_len)
+        return self.interpolate_f0(self.resize_f0(f0, p_len))
