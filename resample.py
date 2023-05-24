49a3b0f2ae35d6c1b1851ba9f28006f52d5b0452
ylzz1997 <ylzz1997@outlook.com>
2023-05-25 01:58:24 +0800

    Restore Loud match logic

--- resample.py
+++ resample.py
@@ -5,7 +5,6 @@ import numpy as np
 from multiprocessing import Pool, cpu_count
 from scipy.io import wavfile
 from tqdm import tqdm
-import pyloudnorm as pyln
 
 
 def process(item):
@@ -22,19 +21,13 @@ def process(item):
             wav = 0.98 * wav / peak
         wav2 = librosa.resample(wav, orig_sr=sr, target_sr=args.sr2)
         wav2 /= max(wav2.max(), -wav2.min())
-        try:
-            meter = pyln.Meter(args.sr2)
-            loudness = meter.integrated_loudness(wav2)
-            wav2 = pyln.normalize.loudness(wav2, loudness, -23.0)
-            # wav2 /= max(wav2.max(), -wav2.min())
-            save_name = wav_name
-            save_path2 = os.path.join(args.out_dir2, speaker, save_name)
-            wavfile.write(
-                save_path2,
-                args.sr2,
-                (wav2 * np.iinfo(np.int16).max).astype(np.int16))
-        except ValueError as e:
-            print(f"{wav_path} is too short(<400ms), the wav skip")
+        save_name = wav_name
+        save_path2 = os.path.join(args.out_dir2, speaker, save_name)
+        wavfile.write(
+            save_path2,
+            args.sr2,
+            (wav2 * np.iinfo(np.int16).max).astype(np.int16)
+        )
 
 
 
