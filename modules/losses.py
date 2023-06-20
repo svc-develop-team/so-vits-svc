dff5ef5e564473e3255dbca2f3b76146be1601b8
YuriHead <ylzz1997@outlook.com>
2023-06-20 10:14:33 +0800

    Update losses.py
    Change back to the previous loss, and do not change the loss until it is confirmed that this can work normally

--- modules/losses.py
+++ modules/losses.py
@@ -55,8 +55,7 @@ def kl_loss(z_p, logs_q, m_p, logs_p, z_mask):
   z_mask = z_mask.float()
   #print(logs_p)
   kl = logs_p - logs_q - 0.5
-  # kl += 0.5 * ((z_p - m_p)**2) * torch.exp(-2. * logs_p)
-  kl += 0.5 * (torch.exp(2.*logs_q)+(z_p - m_p)**2) * torch.exp(-2. * logs_p)
+  kl += 0.5 * ((z_p - m_p)**2) * torch.exp(-2. * logs_p)
   kl = torch.sum(kl * z_mask)
   l = kl / torch.sum(z_mask)
   return l
