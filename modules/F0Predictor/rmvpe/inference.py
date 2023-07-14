b968946c89297c92fec9d82fed1221de33a0f3de
quicksand <quicksandzn@gmail.com>
2023-07-14 17:32:14 +0800

    Update inference.py

--- modules/F0Predictor/rmvpe/inference.py
+++ modules/F0Predictor/rmvpe/inference.py
@@ -16,7 +16,7 @@ class RMVPE:
         else:
             self.device = device
         model = E2E0(4, 1, (2, 2))
-        ckpt = torch.load(model_path, map_location=torch.device(device))
+        ckpt = torch.load(model_path, map_location=torch.device(self.device))
         model.load_state_dict(ckpt['model'])
         model = model.to(dtype).to(self.device)
         model.eval()
