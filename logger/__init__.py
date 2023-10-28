24a840c30f4ff351c4de2b1a36acba98d49e6675
HuanLinOTO <linsmc@126.com>
2023-10-28 15:30:07 +0800

    chore(logger): train

--- logger/__init__.py
+++ logger/__init__.py
@@ -3,37 +3,72 @@ from loguru import logger
 from rich.console import Console
 from rich.logging import RichHandler
 from rich.progress import Progress as _Progress
-from rich.progress import Progress, TextColumn, BarColumn, TimeElapsedColumn, TimeRemainingColumn
+from rich.progress import (
+    Progress,
+    TextColumn,
+    BarColumn,
+    TimeElapsedColumn,
+    TimeRemainingColumn,
+)
 
 import time
+
+import os
+import datetime
 
 console = Console(stderr=None)
 
 logger.remove()
 
-logger.add(lambda m: console.print(m, end=""),
-            format="[green]{time:YYYY-MM-DD HH:mm:ss.SSS}[/green] | "
-                   "[level]{level: <8}[/level] | "
-                   "[cyan]{name}[/cyan]:[cyan]{function}[/cyan]:[cyan]{line}[/cyan] - [level]{message}[/level]",
-            colorize=True)
+
+def format_level(str, length):
+    if len(str) < length:
+        str = str + " " * (length - len(str))
+    else:
+        str = str
+    # 给 str 上对应 level 的颜色
+    if str == "INFO   ":
+        str = f"[bold green]{str}[/bold green]"
+    elif str == "WARNING":
+        str = f"[bold yellow]{str}[/bold yellow]"
+    elif str == "ERROR  ":
+        str = f"[bold red]{str}[/bold red]"
+    elif str == "DEBUG  ":
+        str = f"[bold cyan]{str}[/bold cyan]"
+    return str
+
+def default_format(record):
+    # print(record)
+    return f"[green]{record['time'].strftime('%Y-%m-%d %H:%M:%S')}[/green] | [level]{format_level(record['level'].name,7)}[/level] | [cyan]{record['file'].path.replace(os.getcwd()+os.sep,'')}[/cyan] - [level]{record['message']}[/level]\n"
+
+
+logger.add(lambda m: console.print(m, end=""), format=default_format, colorize=True)
+
 
 def addLogger(path):
-       logger.add(path, format="{time:YYYY-MM-DD HH:mm:ss.SSS} | "
-                                   "{level: <8} | "
-                                   "{name}:{function}:{line} - {message}",
-                                   colorize=True)
+    logger.add(
+        path,
+        format="{time:YYYY-MM-DD HH:mm:ss.SSS} | "
+        "{level: <8} | "
+        "{name}:{function}:{line} - {message}",
+        colorize=True,
+    )
+
 
 info = logger.info
 error = logger.error
 warning = logger.warning
 debug = logger.debug
 
+
 def Progress():
     return _Progress(
-              TextColumn("[progress.description]{task.description}"),
-              # TextColumn("[progress.description]W"),
-              BarColumn(),
-              TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
-              TimeRemainingColumn(),
-              TextColumn("[red]*Elapsed[/red]"),
-              TimeElapsedColumn(),console=console)
+        TextColumn("[progress.description]{task.description}"),
+        # TextColumn("[progress.description]W"),
+        BarColumn(),
+        TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
+        TimeRemainingColumn(),
+        TextColumn("[red]*Elapsed[/red]"),
+        TimeElapsedColumn(),
+        console=console,
+    )
--- test.py
+++ test.py
@@ -3,14 +3,19 @@ import logger
 
 def work(i):
     # logger.info("[red]Working[/red]: {}", i)
-    time.sleep(0.05)
+    # time.sleep(0.)
+    pass
 
 def main():
     logger.info("Before")
     with logger.Progress() as progress:
-        for i in progress.track(range(100)):
+        for i in progress.track(range(100), description="Workkkkkking"):
             work(i)
     logger.info("After")
+    logger.warning("Warning")
+    logger.error("Error")
+    logger.debug("Debug")
+    logger.info("Info")
 
 if __name__ == "__main__":
     main()
